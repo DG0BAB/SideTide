c57a69494fd9c2beae44d87f8fb432eaa258f9e5
Joachim Deelen <jdeelen@micabo.de>
2020-12-06 15:41:13 +0100

    Removed generic base class for delegates Moved initializers to concrete delegate classes Added finalResults methods to the function builder to create concrete delegate objects This fixes #2

--- Sources/TextViewDelegate.swift
+++ Sources/TextViewDelegate.swift
@@ -7,7 +7,8 @@
 
 import UIKit.UITextView
 
-public class TextViewDelegate: TextInputBaseDelegate<UITextView>, UITextViewDelegate {
+@objc public class TextViewDelegate: NSObject, UITextViewDelegate, NSCopying {
+
 	public func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
 		return closureStore.shouldBeginEditing?(textView) ?? true
 	}
@@ -35,4 +36,29 @@ public class TextViewDelegate: TextInputBaseDelegate<UITextView>, UITextViewDele
 	public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
 		closureStore.shouldInteractWithURL?(textView, URL, characterRange, interaction) ?? true
 	}
+
+	// Satisfying NSCopying
+	public func copy(with zone: NSZone? = nil) -> Any {
+		let copy = TextViewDelegate(other: self)
+		return copy
+	}
+
+	/// Allows initialization by function builder
+	public convenience init(@TextInputDelegateBuilder<UITextView> _ closures: () -> TextViewDelegate) {
+		let delegate = closures()
+		self.init(other: delegate)
+	}
+
+	// Used by the function builder to store the closure store
+	internal init(store: TextInputDelegateClosureStore<UITextView>) {
+		self.closureStore = store
+	}
+
+	// Used by the convenience initializer and copy
+	private init(other: TextViewDelegate) {
+		self.closureStore = other.closureStore
+		super.init()
+	}
+
+	private let closureStore: TextInputDelegateClosureStore<UITextView>
 }
